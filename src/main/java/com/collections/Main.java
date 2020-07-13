33a752c5eb9613d844d039eedadac18a07f1e6e0
Artur Teben <teben.artur99@gmail.com>
2020-07-13 13:24:09 +0300

    Refactoring a section of codebase
    Reduce the number of lines to one, input point of programm

--- src/main/java/com/collections/Main.java
+++ src/main/java/com/collections/Main.java
@@ -1,26 +1,9 @@
 package main.java.com.collections;
 
-import main.java.com.collections.tasks.DivisionByThree;
-import main.java.com.collections.tasks.Fruits;
-import main.java.com.collections.tasks.SimilarElements;
-import main.java.com.collections.tasks.SortList;
+import main.java.com.collections.tasks.Executor;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\t\t\t\t\t\tTASK #1");
-        DivisionByThree numb = new DivisionByThree();
-        numb.division();
-
-        System.out.println("\n\t\t\t\t\t\tTASK #2");
-        Fruits fr1 = new Fruits();
-        fr1.addFruits();
-
-        System.out.println("\n\t\t\t\t\t\tTASK #3");
-        SimilarElements simElem = new SimilarElements();
-        simElem.checkElem();
-
-        System.out.println("\n\t\t\t\t\t\tTASK #4");
-        SortList srtList = new SortList();
-        srtList.printSortedIntElem();
+        Executor.executeApplication();
     }
 }
