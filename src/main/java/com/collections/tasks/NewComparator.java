d28b72c09dca2e3a48f94511f4a42ce9a008f26a
Artur Teben <teben.artur99@gmail.com>
2020-07-13 13:30:54 +0300

    Change comparator logic

--- src/main/java/com/collections/tasks/NewComparator.java
+++ src/main/java/com/collections/tasks/NewComparator.java
@@ -3,8 +3,9 @@ package main.java.com.collections.tasks;
 import java.util.Comparator;
 
 public class NewComparator implements Comparator<Integer>{
+
     @Override
     public int compare(Integer x, Integer y) {
-        return x.compareTo(y);
+        return y - x;
     }
 }
