1628315577e7097fda04c730f95220afd3d42f7c
Artur Teben <teben.artur99@gmail.com>
2020-07-13 13:50:28 +0300

    Refactoring a section of code
    Change ArrayList in List type. Create static method randInitList which randomly initialize List.

--- src/main/java/com/collections/tasks/SimilarElements.java
+++ src/main/java/com/collections/tasks/SimilarElements.java
@@ -1,31 +1,32 @@
 package main.java.com.collections.tasks;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class SimilarElements {
-    public void checkElem() {
+
+    public static void checkElem() {
 
         System.out.println("First number list: ");
-        ArrayList<Integer> numbers1 = new ArrayList<>();
+        List<Integer> numbers1 = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
-            numbers1.add(new Random().nextInt(20));
-
-            System.out.print(numbers1.get(i) + " ");
-        }
+        randInitList(15, numbers1);
 
         System.out.println("\n\nSecond number list: ");
-        ArrayList<Integer> numbers2 = new ArrayList<>();
+        List<Integer> numbers2 = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
-            numbers2.add(new Random().nextInt(20));
-
-            System.out.print(numbers2.get(i) + " ");
-        }
+        randInitList(13, numbers2);
 
         System.out.println("\n\nSimilar elements: ");
         numbers1.retainAll(numbers2);
         System.out.println(numbers1);
+    }
+
+    public static void randInitList(int m, List<Integer> list) {
+        for (int i = 0; i < m; i++) {
+            list.add(new Random().nextInt(20));
+            System.out.print(list.get(i) + " ");
+        }
     }
 }
