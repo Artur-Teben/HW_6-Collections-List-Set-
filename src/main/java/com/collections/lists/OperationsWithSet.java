4e4f5cd879ce40419f4427ee9fa0b6c7bc0432a2
Artur Teben <teben.artur99@gmail.com>
2020-07-18 00:20:35 +0300

    Make method initSet required varargs

--- src/main/java/com/collections/lists/OperationsWithSet.java
+++ src/main/java/com/collections/lists/OperationsWithSet.java
@@ -4,25 +4,10 @@ import main.java.com.collections.comparator.NewComparator;
 
 import java.util.*;
 
-public class OperationsWIthSet {
-
-    public static void printSortedIntElem() {
-        Set<Integer> set = new TreeSet<>(new NewComparator());
-        set.add(22);
-        set.add(11);
-        set.add(55);
-        set.add(44);
-        set.add(33);
-        System.out.println("\nTreeSet sorted by descending:\n" + set);
-    }
-
-    public static Set<Integer> sortByDescending(Set<Integer> set) {
-        
-        return set;
-    }
+public class OperationsWithSet {
 
     public static Set<Integer> initSet(Integer...nums) {
-        Set<Integer> set = new TreeSet<>();
+        Set<Integer> set = new TreeSet<>(new NewComparator());
         Collections.addAll(set, nums);
         return set;
     }
