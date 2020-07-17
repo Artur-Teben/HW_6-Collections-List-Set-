fcc838f96992c839d9100c0c15cf479c5d7d5135
Artur Teben <teben.artur99@gmail.com>
2020-07-18 00:14:39 +0300

    Change name of class, change logic of addFruits method, one method = one action

--- src/main/java/com/collections/lists/OperationsWithFruits.java
+++ src/main/java/com/collections/lists/OperationsWithFruits.java
@@ -1,51 +1,30 @@
 package main.java.com.collections.lists;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class Fruits{
+public class OperationsWithFruits {
     public static final String CHECK_FRUIT = "Orange";
 
-    public static void addFruits(){
-        List<String> firstBasket = new ArrayList<>();
-
-        firstBasket.add("Mango");
-        firstBasket.add("Kiwi");
-        firstBasket.add("Orange");
-        firstBasket.add("Banana");
-        firstBasket.add("Apple");
-        firstBasket.add("Avocado");
-
-        System.out.println("List of fruits in first basket: ");
-        Fruits.printFruits(firstBasket);
-        Fruits.swapFruits(firstBasket);
-
-        System.out.println("\nList of fruits in first basket after checking: ");
-        Fruits.printFruits(firstBasket);
-
-        List<String> secondBasket = new ArrayList<>();
-        secondBasket.add("Tangerine");
-        secondBasket.add("Pear");
-        secondBasket.add("Banana");
-        secondBasket.add("Coconut");
-
-        System.out.println("\nList of fruits in second basket: ");
-        Fruits.printFruits(secondBasket);
-        Fruits.swapFruits(secondBasket);
+    public static List<String> addFruits(String...fruits) {
+        List<String> basketWithFruits = new ArrayList<>();
+        Collections.addAll(basketWithFruits, fruits);
+        return basketWithFruits;
     }
 
-    public static void printFruits(List<String> fruits){
-        for (Object elem: fruits) {
+    public void printFruits(List<String> fruits) {
+        for (Object elem : fruits) {
             System.out.print(elem + " ");
         }
         System.out.println(" ");
     }
 
-    public static void swapFruits(List<String> fruits){
-        if(fruits.contains(CHECK_FRUIT)){
+    public void swapFruits(List<String> fruits) {
+        if (fruits.contains(CHECK_FRUIT)) {
             fruits.set(fruits.indexOf(CHECK_FRUIT), "Grapefruit");
         } else {
-            System.out.println("\nThere is no such fruit");
+            System.out.println("\nThere is no " + CHECK_FRUIT);
         }
     }
 }
