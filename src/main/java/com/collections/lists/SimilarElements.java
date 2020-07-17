f0d586b482e58dfb37da47e5139a93d6804b11ed
Artur Teben <teben.artur99@gmail.com>
2020-07-18 00:22:11 +0300

    Make static variable rand

--- src/main/java/com/collections/lists/SimilarElements.java
+++ src/main/java/com/collections/lists/SimilarElements.java
@@ -5,27 +5,29 @@ import java.util.List;
 import java.util.Random;
 
 public class SimilarElements {
+    private final Random rand = new Random();
 
     public static void checkElem() {
 
         System.out.println("First number list: ");
         List<Integer> numbers1 = new ArrayList<>();
 
-        randInitList(15, numbers1);
+        SimilarElements elem = new SimilarElements();
+        elem.initRandList(15, numbers1);
 
         System.out.println("\n\nSecond number list: ");
         List<Integer> numbers2 = new ArrayList<>();
 
-        randInitList(13, numbers2);
+        elem.initRandList(13, numbers2);
 
         System.out.println("\n\nSimilar elements: ");
         numbers1.retainAll(numbers2);
         System.out.println(numbers1);
     }
 
-    public static void randInitList(int m, List<Integer> list) {
+    public void initRandList(int m, List<Integer> list) {
         for (int i = 0; i < m; i++) {
-            list.add(new Random().nextInt(20));
+            list.add(rand.nextInt(20));
             System.out.print(list.get(i) + " ");
         }
     }
