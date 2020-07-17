3537d65628cac59832539d5af3ba8f585e85a636
Artur Teben <teben.artur99@gmail.com>
2020-07-18 00:24:19 +0300

    Refactoring a specific section of the code

--- src/main/java/com/collections/Executor.java
+++ src/main/java/com/collections/Executor.java
@@ -1,22 +1,41 @@
 package main.java.com.collections;
 
 import main.java.com.collections.lists.DivisionByThree;
-import main.java.com.collections.lists.Fruits;
+import main.java.com.collections.lists.OperationsWithFruits;
 import main.java.com.collections.lists.SimilarElements;
-import main.java.com.collections.lists.SortSetByDescending;
+import main.java.com.collections.lists.OperationsWithSet;
+
+import java.util.List;
+import java.util.Set;
 
 public class Executor {
-    public static void executeApplication() {
+    public void executeApplication() {
         System.out.println("\t\t\t\t\t\tTASK #1");
-        DivisionByThree.division();
+        DivisionByThree.divideByThree();
 
         System.out.println("\n\t\t\t\t\t\tTASK #2");
-        Fruits.addFruits();
+
+        OperationsWithFruits fruits = new OperationsWithFruits();
+        List<String> firstBasket = OperationsWithFruits.addFruits("Mango", "Kiwi", "Orange", "Banana", "Apple", "Avocado");
+
+        System.out.println("List of fruits in first basket: ");
+        fruits.printFruits(firstBasket);
+        fruits.swapFruits(firstBasket);
+
+        System.out.println("\nList of fruits in first basket after checking: ");
+        fruits.printFruits(firstBasket);
+
+        List<String> secondBasket = OperationsWithFruits.addFruits("Tangerine", "Pear", "Banana", "Coconut");
+
+        System.out.println("\nList of fruits in second basket: ");
+        fruits.printFruits(secondBasket);
+        fruits.swapFruits(secondBasket);
 
         System.out.println("\n\t\t\t\t\t\tTASK #3");
         SimilarElements.checkElem();
 
         System.out.println("\n\t\t\t\t\t\tTASK #4");
-        SortSetByDescending.printSortedIntElem();
+        Set<Integer> intSet = OperationsWithSet.initSet(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        OperationsWithSet.printSet(intSet);
     }
 }
