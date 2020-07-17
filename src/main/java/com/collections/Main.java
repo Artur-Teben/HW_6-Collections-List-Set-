27be3f2de37c237cb2a250cfc7995c6e3b7ef9b7
Artur Teben <teben.artur99@gmail.com>
2020-07-18 00:25:35 +0300

    Make an instance Executor class

--- src/main/java/com/collections/Main.java
+++ src/main/java/com/collections/Main.java
@@ -2,6 +2,7 @@ package main.java.com.collections;
 
 public class Main {
     public static void main(String[] args) {
-        Executor.executeApplication();
+        Executor exe = new Executor();
+        exe.executeApplication();
     }
 }
